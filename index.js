39682901c240c49db355aa1d5b1358f40f4d5f4c
desmond <desmond@desmonds-MacBook-Pro.local>
2022-04-12 08:05:51 +0300

    task complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, message) {
+    let myCard = [];
+    for (let i = 0; i < names.length; i++) {
+       myCard.push(`Thank you, ${names[i]}, for the wonderful ${message} gift!`);
+    }
+    return myCard;
+}
+
+function countDown(n) {
+    let count = n;
+    while (count > 0) {
+      console.log(count);
+      count--;
+    } 
+    console.log(count);
+}
